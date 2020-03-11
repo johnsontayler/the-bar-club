93084f504eab772c2590ff1f4cd5a0256f754e09
Tayler Johnson <tayler.johnson@buck.tv>
2020-03-11 14:46:26 -0700

    smooth rotation

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-let i = 0;
-
-// document.addEventListener("mousedown", (evt, movedMouse) => {
-//   const cube = document.getElementById("cube");
-//   // cube.style.transform = `rotateY(${i}deg)`;
-//   console.log(movedMouse.x);
-// });
-
-const cube = document.getElementById("cube");
+const cubeContainer = document.getElementById("container");
+const cubeFaces = document.querySelectorAll(".cube__face");
 
 let mouse = {
     start: {}
   },
   viewport = {
-    x: 20,
-    el: cube,
+    x: 0,
+    el: cubeContainer,
+    elSides: {
+      front: cubeFaces[0],
+      back: cubeFaces[1],
+      right: cubeFaces[2],
+      left: cubeFaces[3]
+    },
     move: coords => {
       if (coords) {
         if (typeof coords.x === "number") viewport.x = coords.x;
@@ -21,7 +20,7 @@ let mouse = {
       viewport.el.style.transform = "rotateY(" + viewport.x + "deg)";
     },
     reset: () => {
-      this.move({ x: 0 });
+      viewport.move({ x: 0 });
     }
   };
 
@@ -42,10 +41,50 @@ const moveViewport = movedMouse => {
   }
 
   viewport.move({
-    x: viewport.x + parseInt((mouse.start.x - movedMouse.x) / 2)
+    x: viewport.x + parseInt(mouse.start.x - movedMouse.x) / 10
   });
 
   mouse.last.x = movedMouse.x;
+};
+
+const handleWallOpacity = () => {
+  const map = (e, t, n, i, o) => {
+    return i + (o - i) * ((e - t) / (n - t));
+  };
+
+  let y = Math.floor(viewport.x / 90) % 4,
+    v = Math.abs(viewport.x % 360);
+  viewport.x < 0 && ((y = 4 - Math.abs(y)), 4 == y && (y = 0), (v = 360 - v));
+  let g = 0.2;
+  switch (y) {
+    case 0:
+      // let b = map(v, 0, 90, 1, g);
+      // viewport.elSides.front.style.opacity = 1;
+      // let b = map(v, 0, 90, g, 1);
+      // (viewport.elSides.right.style.opacity = b),
+      //   (viewport.elSides.left.style.opacity = g);
+      break;
+    // case 1:
+    //   let b = map(v, 90, 180, 1, g);
+    //   viewport.elSides.right.style.opacity = 1;
+    //   let b = map(v, 90, 180, g, 1);
+    //   viewport.elSides.front.style.opacity = b,
+    //     viewport.elSides.left.style.opacity = g;
+    //   break;
+    // case 2:
+    //   let b = map(v, 180, 270, 1, g);
+    //   viewport.elSides.front.style.opacity = 1;
+    //   let b = map(v, 180, 270, g, 1);
+    //   viewport.elSides.left.style.opacity = b,
+    //     viewport.elSides.back.style.opacity = g;
+    //   break;
+    // case 3:
+    //   let b = map(v, 270, 360, 1, g);
+    //   viewport.elSides.left.style.opacity = 1;
+    //   let b = map(v, 270, 360, g, 1);
+    //   viewport.elSides.back.style.opacity = b,
+    //     viewport.elSides.right.style.opacity = g;
+  }
 };
 
 document.addEventListener("mousedown", evt => {
@@ -59,11 +98,12 @@ document.addEventListener("mousedown", evt => {
       moveViewport({
         x: event.pageX
       });
+      handleWallOpacity();
     })
   );
 
   document.addEventListener("mouseup", () => {
     document.removeEventListener("mousemove", mouseMove);
-    viewport.reset();
+    // viewport.reset();
   });
 });
